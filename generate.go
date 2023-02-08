5469259f239e95c4fc3616482050deb83373b067
Jack Allen <jackallensemail+git@gmail.com>
2023-02-08 14:56:52 +0000

    generated

--- generate.go
+++ generate.go
@@ -1,3 +1,3 @@
 package render
 
-//go:generate openapi-generator generate -i ./api/openapi.yaml -g go -o . -p "packageName=render" --git-user-id jackall3n --git-repo-id=render-go
+//go:generate openapi-generator-cli generate -i ./api/openapi.yaml -g go -o . -p "packageName=render" --git-user-id jackall3n --git-repo-id=render-go
