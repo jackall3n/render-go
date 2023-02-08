0101fc469a1308df99bf848c3e11fb0924a0fcf5
Jack Allen <jackallensemail+git@gmail.com>
2023-02-08 14:46:52 +0000

    generated

--- generate.go
+++ generate.go
@@ -1,4 +1,3 @@
 package render
 
-//go:generate-old oapi-codegen --package=render -generate=types,client -o ./render.go ./openapi.yaml
 //go:generate openapi-generator generate -i ./api/openapi.yaml -g go -o . -p "packageName=render,isGoSubmodule=yes" --git-user-id jackall3n --git-repo-id=render-go
